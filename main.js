71a87be68b58780319fd451649adc368398c6900
chinmay171 <72167507+chinmay171@users.noreply.github.com>
2022-02-28 20:49:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+let treeObj = require('./commands/tree')
+let helpObj = require('./commands/help')
+let organizeObj = require('./commands/organize')
+let inputarr = process.argv.slice(2)
+let command = inputarr[0]
+
+let types = {
+    media: ["mp4", "mkv", "mp3"],
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: [
+        "csv",
+      "docx",
+      "doc",
+      "pdf",
+      "xlsx",
+      "xls",
+      "odt",
+      "ods",
+      "odp",
+      "odg",
+      "odf",
+      "txt",
+      "ps",
+      "tex",
+    ],
+    app: ["exe", "dmg", "pkg", "deb", ],
+    photos : ["jpg", "jpeg" , "png"],
+  };
+
+switch(command){
+    case 'tree' :
+        treeObj.treeKey(inputarr[1])
+        break
+    case 'organize' :
+        organizeObj.organizeKey(inputarr[1])
+        break
+    case 'help' :
+        helpObj.helpKey()
+        break
+    default :
+    console.log("enter valid command please or atleast consider help...")     
+}
